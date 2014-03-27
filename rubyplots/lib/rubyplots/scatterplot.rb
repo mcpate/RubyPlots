c4db11aa4e9ad81b85b786f6ff8cf926054e5a9e
Matthew Pate <mcpate@gmail.com>
2014-03-27 15:06:07 -0700

    Adds two test files (one for data and one for latex) and adds some more details to the way scatterplot writes data.

--- rubyplots/lib/rubyplots/scatterplot.rb
+++ rubyplots/lib/rubyplots/scatterplot.rb
@@ -1,3 +1,5 @@
+require 'pry'
+require 'csv'
 
 class ScatterPlot
 
@@ -9,14 +11,21 @@ class ScatterPlot
 
   private
 
+  # This reads the datafile in a way that requires a certain format.
+  # The title of this scatterplot is the name of the file (minus the extension).
+  # The first line of the file must be the names of the x and y columns.
   def writeScatterPlot(dataFile, latexFile)
-    File.open(latexFile, "a") do |file|
-      #file << "\tikzsetnextfilename{#{File.basename(dataFile, 
-      file << '\\begin{tikzpicture}' + "\n"
-	    file << '\\begin{axis}[]' + "\n"
-	    file << '\\addplot table [only marks, x=x, y=y] ' + "{#{dataFile}};\n"
-	    file << '\\end{axis}' + "\n"
-      file << '\\end{tikzpicture}' + "\n"
+    CSV.open(dataFile, "r", {:col_sep => "\t"}) do |data|
+      colNames = data.readline
+
+      File.open(latexFile, "a") do |file|
+        file << '\\tikzsetnextfilename' + "{#{File.basename(dataFile, ".scatterplot")}}" 
+        file << '\\begin{tikzpicture}' + "\n"
+	      file << '\\begin{axis}[]' + "\n"
+	      file << '\\addplot table [only marks, ' + "x=#{colNames[0]}, y=#{colNames[1]}] {#{dataFile}};\n"
+	      file << '\\end{axis}' + "\n"
+        file << '\\end{tikzpicture}' + "\n"
+      end
     end
   end
 
